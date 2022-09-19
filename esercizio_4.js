9d8882da9f56ce8632c8f518cf7b42080dbd9da4
Leonardo_Lo_medico <leonardo.lomedico@hotmail.it>
2022-09-19 13:21:01 +0200

    esercizio4

---
+++ esercizio_4.js
@@ -0,0 +1,12 @@
+const prompt = require('prompt-sync')();
+let ins = prompt("inserire un numero intero: ");
+ let sum =0;
+for(let i=0;i<=ins;i++){
+    
+   
+    sum = sum +i;
+    
+    //console.log(i);
+    console.log(sum);
+}
+console.log("La somma finale è: "+ sum);
