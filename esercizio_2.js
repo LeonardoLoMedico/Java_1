fc930a90f2f2905f2f3554ee43a028b034a5c6a0
Leonardo_Lo_medico <leonardo.lomedico@hotmail.it>
2022-09-20 20:56:31 +0200

    esrcizio2_conbonus

--- esercizio_2.js
+++ esercizio_2.js
@@ -1,10 +1,19 @@
 const prompt = require('prompt-sync')();
 //let ins =prompt(" Scrivi e il tuo nome e la tua età: ");
 //console.log(ins);
-let name =prompt ("Scrivi il tuo nome: ");
-let age = prompt ("inserisci la tua età: ");
+let name =prompt ("Scriva il suo nome: ");
+let surname=prompt("Scriva il suo cognome: ")
+let age = prompt ("Inserisca la tua età: ");
 console.log(name + " "+ age);
-if(age>18){console.log("sei maggiorenne,puoi votare")};
+
+if(age>18){
+    console.log("Lei é maggiorenne,può votare")
+    
+    
+        let num= Math.floor((Math.random()*10));
+        console.log("cabina: "+ num);
+
+};
 if (age<18){ cosole.log("Devi avere 18 anni per votare.")};
 
 
