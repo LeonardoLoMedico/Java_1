a1a07f6a2e2b2ed9e5aaa44ae08bf1b21f40f840
Leonardo_Lo_medico <leonardo.lomedico@hotmail.it>
2022-09-20 21:12:36 +0200

    esercizio_4_conbonus

--- esercizio_4.js
+++ esercizio_4.js
@@ -1,6 +1,8 @@
 const prompt = require('prompt-sync')();
 let ins = prompt("inserire un numero intero: ");
 let sum =0;
+let molt=1;
+
 for(let i=0;i<=ins;i++){
     
    
@@ -10,3 +12,12 @@ for(let i=0;i<=ins;i++){
     console.log(sum);
 }
 console.log("La somma finale è: "+ sum);
+for(let i=1;i<=ins;i++){
+    
+   
+    molt=molt*i;
+    
+    //console.log(i);
+    console.log(molt);
+}
+console.log("La moltiplicazione finale è: "+ molt);
