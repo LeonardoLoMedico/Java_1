f4514a9d1c1e6259937b68f3b1a98d890eb18afb
Leonardo_Lo_medico <leonardo.lomedico@hotmail.it>
2022-09-21 20:13:07 +0200

    esercizio6_conbonus

--- esercizio_6.js
+++ esercizio_6.js
@@ -1,15 +1,32 @@
 const prompt = require('prompt-sync')();
+console.log("Per effettuare la somma premere + \nPer effettuare la sottrazzione premere - \nPer effettuare la moltiplicazione premere * \nPer effettuare la divisione premere / ");
+console.log("Per effettuare radice quadrata premere ^ \nPer effettuare la radice quadrata premere sqrt ");
+let operazione = prompt("Che operazione vuoi effettuare ?  ");
+if(operazione=="^"||operazione=="sqrt"){
+    if(operazione=="^"){
+    let base=prompt("Inserire la base della potenza: ");
+    let esp=prompt("Inserire l'esponente della potenza: ");
+    let pot=Math.pow(base, esp);
+    console.log("La potenza di "+base+"^"+esp+" è: "+pot);
+    }
+    if(operazione=="sqrt"){
+    let sqrt=prompt("Inserire il numere di cui si vule la radice quadrata: ");
+    let rad=Math.sqrt(sqrt);
+    console.log("La radice quadrata è: "+rad);
+    }
+
+
+}else{
 let num1= prompt("Inserisci un numero: ");
 let num2= prompt("Inserire secondo numero: ");
 num1=Number(num1);
 num2=Number(num2);
-console.log("Per effettuare la somma premere 1 \nPer effettuare la sottrazzione premere 2 \nPer effettuare la moltiplicazione premere 3 \nPer effettuare la divisione premere 4 \nPer effettuare radice quadrata premere 5 ")
-let operazione = prompt("Che operazione vuoi effettuare ?  ");
 let sum= num1+ num2;
 let diff= num1-num2;
 let div=num1/num2;
 let molt= num1*num2;
-if(operazione==1){ console.log("La somma dei due numeri è: " + sum)};
-if(operazione==2){console.log("La sottrazione dei due numeri è: "+ diff)};
-if(operazione==3){console.log("La moltiplicazione dei due numeri è: "+ molt)};
-if(operazione==4){console.log("La divisione dei due numeri è: "+div)};
+if(operazione=="+"){ console.log("La somma dei due numeri è: " + sum)};
+if(operazione=="-"){console.log("La sottrazione dei due numeri è: "+ diff)};
+if(operazione=="*"){console.log("La moltiplicazione dei due numeri è: "+ molt)};
+if(operazione=="/"){console.log("La divisione dei due numeri è: "+div)};
+}
