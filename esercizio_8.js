43c40e86f56bbf04fc2dc1730171c8bb6added04
LeonardoLoMedico <leonardo.lomedico@hotmail.it>
2022-09-22 13:34:51 +0200

    iniziobonus8

--- esercizio_8.js
+++ esercizio_8.js
@@ -1,6 +1,8 @@
 const prompt = require('prompt-sync')();
 let ins = prompt("Inserisci numero intero: ");
 console.log("I numeri primi fino a "+ins+ " sono: ");
+let arr =[ ];
+let compara = [ ];
 for(i=1;i<=ins;i++){
 
     let count =0;
@@ -14,5 +16,35 @@ for(i=1;i<=ins;i++){
         } 
     }
 
-    if(count==2){console.log(i )}
+    if(count==2){//console.log(i);
+    //inserisco i numeri trovati in un array
+    arr.push(i)
+    console.log(arr);
+  }
+
 }
+let ins1=prompt("Inserisci un numero: ");
+
+for(i=1;i<=ins;i++){
+
+  let count =0;
+
+  for(k=1;k<=i;k++){
+
+      if(i%k==0){
+
+        count++;
+        
+      } 
+  }
+
+  if(count==2){//console.log(i);
+  //inserisco i numeri trovati in un array
+  arr.push(i)
+  console.log(arr);
+}
+
+}
+
+//prendendo in input un altro numero scrivete la somma fino al numero piu' vicino
+// devo fare la somma dei numeri primi fino al numero primo pù vicino a quello inserito, il numero più vicino può anche essere maggiore di quello inserito
