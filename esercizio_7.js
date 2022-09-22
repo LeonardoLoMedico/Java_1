509373c748b0b780c45a2802d897420be1e8f555
LeonardoLoMedico <leonardo.lomedico@hotmail.it>
2022-09-22 12:17:38 +0200

    esercizio_7_conbonus

--- esercizio_7.js
+++ esercizio_7.js
@@ -1,14 +1,26 @@
 const prompt = require('prompt-sync')();
-let ins = prompt("inserire un numero intero maggiore di 1 : ");
+let ins = prompt("Inserire un numero : ");
+// erore se numero è = 0 or 2
+if(ins<2){
+    console.log("Inserire un numero intero maggiore di 1!");
+  
+}else{
+
+// voglio permettere all'utente di scegliere quale tabellina non visualizzare
+let ins_1 = prompt("Inserire la tabellina che non vuoi visualizzare : ");
 let molt = 0;
 while(ins!=1){
-    console.log("La tabellina di: "+ ins);
+    console.log("La tabellina di "+ ins+ ":");
 for (i=0; i<=10; i++){
     
     molt = i*ins;
 
-    console.log(molt);
+    console.log(ins+" * "+i+" = " +molt);
    }
   
    ins--;
+   if(ins_1==ins){
+    ins--;
+   }
+}
 }
